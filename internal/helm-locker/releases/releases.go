f3a180c13339de1bac969b44157f634b3d041094
Alejandro Moreno <alejandro.moreno@suse.com>
2026-06-01 13:12:48 +0200

    Add a logging adapter to keep loogging in Helm v4

--- internal/helm-locker/releases/releases.go
+++ internal/helm-locker/releases/releases.go
@@ -1,14 +1,46 @@
 package releases
 
 import (
+	"context"
 	"fmt"
+	"log/slog"
 	"sync"
 
+	"github.com/sirupsen/logrus"
 	releasev1 "helm.sh/helm/v4/pkg/release/v1"
 	"helm.sh/helm/v4/pkg/storage"
 	"helm.sh/helm/v4/pkg/storage/driver"
 	"k8s.io/client-go/kubernetes"
 )
+
+// logrusHandler bridges Helm v4's slog-based storage logger to logrus.
+type logrusHandler struct{}
+
+func (h *logrusHandler) Enabled(_ context.Context, level slog.Level) bool {
+	return level >= slog.LevelDebug
+}
+
+func (h *logrusHandler) Handle(_ context.Context, r slog.Record) error {
+	msg := r.Message
+	r.Attrs(func(a slog.Attr) bool {
+		msg += " " + a.Key + "=" + a.Value.String()
+		return true
+	})
+	switch {
+	case r.Level >= slog.LevelError:
+		logrus.Errorf("%s", msg)
+	case r.Level >= slog.LevelWarn:
+		logrus.Warnf("%s", msg)
+	case r.Level >= slog.LevelInfo:
+		logrus.Infof("%s", msg)
+	default:
+		logrus.Debugf("%s", msg)
+	}
+	return nil
+}
+
+func (h *logrusHandler) WithAttrs(_ []slog.Attr) slog.Handler { return h }
+func (h *logrusHandler) WithGroup(_ string) slog.Handler      { return h }
 
 type HelmReleaseGetter interface {
 	Last(namespace, name string) (*releasev1.Release, error)
@@ -36,6 +68,7 @@ func (g *latestReleaseGetter) getStore(namespace string) *storage.Storage {
 		return store
 	}
 	store = storage.Init(driver.NewSecrets(g.K8s.CoreV1().Secrets(namespace)))
+	store.SetLogger(&logrusHandler{})
 	g.namespacedStorage[namespace] = store
 	return store
 }
